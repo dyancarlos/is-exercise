1bc1b70fccc5cb700f6f226e8909268f3c4ea695
Dyan Carra <dyancarra@gmail.com>
2017-03-08 22:41:14 -0300

    config enum

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ module IsExercise
   class Application < Rails::Application
     # Use the responders controller from the responders gem
     config.app_generators.scaffold_controller :responders_controller
-
+    config.autoload_paths += %W(#{config.root}/app/enumerations)
     config.i18n.default_locale = "pt-BR"
     config.time_zone = "Brasilia"
     config.encoding = "utf-8"
