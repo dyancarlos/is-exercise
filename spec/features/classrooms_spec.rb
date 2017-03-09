b31d0deb43eb3adcf06bade0592e53a8f0769287
Dyan Carra <dyancarra@gmail.com>
2017-03-09 20:42:33 -0300

    stundents features test

---
+++ spec/features/classrooms_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe "Classrooms Feature", type: :feature do
+end
---
+++ spec/features/students_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe "Students Feature", type: :feature do
+
+  it "create a valid student" do
+    visit new_student_path
+
+    fill_in "Nome", with: "Ingles"
+    fill_in "Matrícula", with: "1234567890"
+    select "Ativo", from: "Status"
+    click_button "Salvar"
+
+    expect(page).to have_content "Estudante foi criado com sucesso"
+  end
+
+  it "create with a blank name" do
+    visit new_student_path
+
+    fill_in "Nome", with: ""
+    fill_in "Matrícula", with: "1234567890"
+    select "Ativo", from: "Status"
+    click_button "Salvar"
+
+    expect(page).to have_content "Nome não pode ficar em branco"
+  end
+
+  it "create with a blank register_number" do
+    visit new_student_path
+
+    fill_in "Nome", with: "Ingles"
+    fill_in "Matrícula", with: ""
+    select "Ativo", from: "Status"
+    click_button "Salvar"
+
+    expect(page).to have_content "Matrícula não pode ficar em branco"
+  end
+
+  it "create with blank fields" do
+    visit new_student_path
+
+    fill_in "Nome", with: ""
+    fill_in "Matrícula", with: ""
+    click_button "Salvar"
+
+    expect(page).to have_content "Nome não pode ficar em branco"
+    expect(page).to have_content "Matrícula não pode ficar em branco"
+  end
+
+end
