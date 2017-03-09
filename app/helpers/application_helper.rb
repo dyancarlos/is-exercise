cfd529da237085285450bccb2898de76fd479c15
Dyan Carra <dyancarra@gmail.com>
2017-03-08 22:21:40 -0300

    add validation messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def validation(object)
+    return if object.nil?
+    unless object.errors.empty?
+      html = content_tag(:div, content_tag(:h1, "Aconteceu algo de Errado!"))
+      object.errors.full_messages.each { |msg| html << content_tag(:li, msg) }
+      content_tag(:div, html, class: "validation").html_safe
+    end
+  end
 end
